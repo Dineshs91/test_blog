838634aacc38ce3f98a30cd8a4cad274fe5d60a8
dinesh <dsdiscoverdinesh@gmail.com>
2014-09-17 21:52:22 +0530

    Fixing improper validation by using validators

--- blogg/login/forms.py
+++ blogg/login/forms.py
@@ -1,16 +1,37 @@
 from django import forms
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 
+def validate_input(value):
+    if len(value.strip()) == 0:
+        raise ValidationError('field empty')
 
 class UserRegisterForm(forms.ModelForm):
     """
     User registration form. All fields are required.
     """
-    first_name = forms.CharField(max_length=15, help_text="Firstname")
-    last_name = forms.CharField(max_length=15, help_text="Lastname")
+    first_name = forms.CharField(
+        max_length=15, 
+        help_text="Firstname",
+        validators=[validate_input]
+        )
+    last_name = forms.CharField(
+        max_length=15, 
+        help_text="Lastname",
+        validators=[validate_input]
+        )
     email = forms.EmailField(help_text='Email ID')
-    username = forms.CharField(help_text='Username', max_length=15)
-    password = forms.CharField(widget=forms.PasswordInput, max_length=15, help_text="Password")
+    username = forms.CharField(
+        help_text='Username', 
+        max_length=15,
+        validators=[validate_input]
+        )
+    password = forms.CharField(
+        widget=forms.PasswordInput, 
+        max_length=15, 
+        help_text="Password",
+        validators=[validate_input]
+        )
     
     class Meta:
         model = User
--- blogg/posts/forms.py
+++ blogg/posts/forms.py
@@ -1,9 +1,23 @@
 from django import forms
 from posts.models import UserPosts
+from django.core.exceptions import ValidationError
+
+def validate_input(value):
+    if len(value.strip()) == 0:
+        raise ValidationError('field empty')
 
 class UserPostForm(forms.ModelForm):
-	post_title = forms.CharField(max_length=100, help_text="Please enter the title")
-	post_content = forms.CharField(widget=forms.Textarea)
+	post_title = forms.CharField(
+        max_length=100,
+        help_text="Please enter the title",
+        validators=[validate_input],
+        error_messages={'required':'Post title is required', 'invalid':'Enter a valid post title'}
+        )
+	post_content = forms.CharField(
+        widget=forms.Textarea, 
+        validators=[validate_input],
+        error_messages={'required':'Post content is required', 'invalid':'Add some content'}
+        )
 
 	class Meta:
 		model = UserPosts
